a78e8b6559d554307960bcebcdd9efc93081be5e
Chauncey Hoover <dealernextdoor@gmail.com>
2018-07-19 13:27:25 -0400

    Committing to move to my other computer again
    Started working on filter menu design

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,8 @@ require('dotenv').load();
 
 const connection = require('mysql').createPool({
     multipleStatements: true,
-    host: process.env.SQL_SERVER,
-    user: process.env.SQL_USER,
+    host:     process.env.SQL_SERVER,
+    user:     process.env.SQL_USER,
     password: process.env.SQL_PASS,
     database: process.env.SQL_DB
 });
