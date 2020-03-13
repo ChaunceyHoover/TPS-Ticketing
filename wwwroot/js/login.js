5cd9be5ed7384d508b5bbb4423cc379bdf859fb2
Chauncey Hoover <dealernextdoor@gmail.com>
2020-03-12 21:48:34 -0400

    Uploading to my github
    Moved repo from old TPS repo to my repo.

--- wwwroot/js/login.js
+++ wwwroot/js/login.js
@@ -1,17 +1,38 @@
 var current = 1;
 
+// setInterval(function() {
+// 	current++;
+// 	switch(current) {
+// 		case 1:
+// 			$("html").css("background", "url('/img/background-1.png') no-repeat fixed center center");
+// 			break;
+// 		case 2:
+// 			$("html").css("background", "url('/img/background-2.png') no-repeat fixed center center");
+// 			break;
+// 		case 3:
+// 			$("html").css("background", "url('/img/background-3.png') no-repeat fixed center center");
+// 			break;
+// 		default:		
+// 			location.reload();
+// 			break;
+// 	}
+// }, 3000);
+
+
+setTimeout(function() { location.reload() }, 10000);
+
+var counter = 0;
 setInterval(function() {
-	current++;
-	switch(current) {
-		case 1:
-			$("html").css("background", "url('/img/background-1.png') no-repeat fixed center center");
-			break;
-		case 2:
-			$("html").css("background", "url('/img/background-2.png') no-repeat fixed center center");
-			break;
-		default:
-			$("html").css("background", "url('/img/background-3.png') no-repeat fixed center center");
-			current = 0;
-			break;
-	}
-}, 3000);
+	var circle = $("<div class='shape' id='circle" + (++counter) + "'></div>");
+	var radius = Math.round(Math.random() * 100) + 300;
+	circle.css('width', radius + 'px');
+	circle.css('height', radius + 'px');
+	circle.css('border-radius', (radius / 2) + "px");
+	circle.css('background-color', 'red');
+	circle.css('left', (Math.random() * (1920 - (radius / 2))) + 'px');
+	circle.css('top', (Math.random() * (1080 - (radius / 2))) + 'px');
+	circle.css('position', 'absolute');
+	circle.css('filter', 'blur(100px)').css('oFilter', 'blur(100px)');
+
+	$('#shape-container').append(circle);
+}, 2000);
